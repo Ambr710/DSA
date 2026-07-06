573be3217701ccb217aec22f491db16da409223c
Ambrish Gupta <156555352+Ambr710@users.noreply.github.com>
2026-07-06 13:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (79.16%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i=0;i<n+1;i++){
-            res.push_back(__builtin_popcount(i));
-        }
-        return res;
-        
+      vector<int> res(n+1);
+      for(int i=0;i<n+1;i++){
+        if(i%2!=0) res[i]=res[i/2]+1;
+        else res[i]=res[i/2];
+      }
+      return res;
     }
 };
