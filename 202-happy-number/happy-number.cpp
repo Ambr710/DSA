3b1682aa4faaf31d88d2161d0d01c77d3a84ce55
Ambrish Gupta <156555352+Ambr710@users.noreply.github.com>
2026-09-12 10:18:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (67.02%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int getnext(int n){
+    int sum=0;
+    while(n>0){
+        int ld=n%10;
+        sum+=ld*ld;
+        n/=10;
+    }
+    return sum;
+}
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+         do{
+            slow=getnext(slow);
+            fast=getnext(getnext(fast));
+         }
+         while(slow!=fast) ;
+         return slow==1;
+    }
+};
